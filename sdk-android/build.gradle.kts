dd30fb456a86b754296a80aa7af343bfc7ce56e8
XxZIOIMIBIExX <personal.encryption@aleeas.com>
2026-05-31 11:53:22 -0700

    build: pin android and kotlin plugin versions in sdk-android build script

--- sdk-android/build.gradle.kts
+++ sdk-android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.android.library") version "8.2.2"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
 android {
